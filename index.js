c34247ca6495143e4b250527e05298b9df0b43e3
tamzid620 <tamzid620@gmail.com>
2023-05-14 11:04:03 +0600

    vercel added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const jwt   = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
@@ -21,16 +21,16 @@ const client = new MongoClient(uri, {
   }
 });
 
-const verifyJWT = ( req, res , next ) => {
+const verifyJWT = (req, res, next) => {
   const authorization = req.headers.authorization;
-  if(!authorization){
-    return res.status(401).send({error: true , message: 'unauthorized access'})
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' })
   }
   const token = authorization.split(' ')[1];
-  console.log('token inside vetyfy JWT' , token);
-  jwt.verify( token, process.env.ACCESS_TOKEN_SECRET , (error, decoded) => {
-    if(error){
-      return res.status(403).send({error: true, message: 'unathorized access'})
+  console.log('token inside vetyfy JWT', token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(403).send({ error: true, message: 'unathorized access' })
     }
     req.decoded = decoded;
     next();
@@ -46,21 +46,22 @@ async function run() {
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
     // JWT 
-    app.post('/jwt' , (req, res) => {
+    app.post('/jwt', (req, res) => {
       const user = req.body;
       console.log(user);
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn:'1h'
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '1h'
       });
-      res.send({token});
+      res.send({ token });
     })
 
     // services -----------------------------
     app.get('/services', async (req, res) => {
+      const serviceCollection = client.db('carDoctor').collection('services');
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
-    })
+    });
 
     app.get('/services/:id', async (req, res) => {
       const id = req.params.id;
@@ -77,21 +78,20 @@ async function run() {
 
     // bookings -----------------------------
 
-    app.get('/bookings', verifyJWT,  async (req, res) => {
-      const decoded = req.decoded;
-      console.log('came back after verify', decoded)
-
-      if(decoded.email !== req.query.email){
-         return res.status(403).send({error:1, message: 'forbidden access'})
-      }
-
-      let query = {};
-      if (req.query?.email) {
-        query = { email: req.query.email }
-      }
-      const result = await bookingCollection.find(query).toArray();
-      res.send(result);
-    })
+    app.get('/bookings', verifyJWT, async (req, res) => {
+  const decoded = req.decoded;
+  console.log('came back after verify', decoded);
+  if (decoded.email !== req.query.email) {
+    return res.status(403).send({ error: 1, message: 'forbidden access' });
+  }
+  let query = {};
+  if (req.query?.email) {
+    query = { email: req.query.email };
+  }
+  const bookingCollection = client.db('carDoctor').collection('bookings');
+  const result = await bookingCollection.find(query).toArray();
+  res.send(result);
+});
 
     app.post('/bookings', async (req, res) => {
       const booking = req.body;
@@ -106,13 +106,13 @@ async function run() {
       const updatedBooking = req.body;
       console.log(updatedBooking);
       const updateDoc = {
-          $set: {
-              status: updatedBooking.status
-          },
+        $set: {
+          status: updatedBooking.status
+        },
       };
       const result = await bookingCollection.updateOne(filter, updateDoc);
       res.send(result);
-  })
+    })
 
     app.delete('/bookings/:id', async (req, res) => {
       const id = req.params.id;
